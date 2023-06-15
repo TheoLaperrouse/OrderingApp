661acaa5327b19e927c4c6d9b61c1fd2b0dad10f
Théo Laperrouse <theolaperrouse@gmail.com>
2023-06-15 23:17:35 +0200

    Init OrderingApp

---
+++ back/src/main.py
@@ -0,0 +1,28 @@
+import logging
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from src.routers import drinks
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def init_routers(_app):
+    '''Initialize routers'''
+    routers = [drinks]
+    for router in routers:
+        _app.include_router(router.router)
+
+
+app = FastAPI()
+
+init_routers(app)
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    '''Redirect to docs on /'''
+    return RedirectResponse(url='/docs')
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
